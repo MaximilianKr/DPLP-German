27444c20051615ce4fd20b1879f628446edd278d
Max <przvlprd@pm.me>
2025-09-02 12:06:26 +0200

    feat: optimize edu segmenter pipeline

--- run_seg_pipeline.py
+++ run_seg_pipeline.py
@@ -3,16 +3,19 @@ import argparse
 import subprocess
 
 
-def run_local_preprocessing(input_path):
-    os.system(f"python3 ger_3_ner.py {input_path}")
+def run_optimized_preprocessing(input_path):
+    # Run the consolidated Stanza script for NER and CoNLL
+    subprocess.run([
+        "python3", "run_stanza_preprocessing.py", input_path
+    ])
+    # Run the BerkeleyParser script separately
     os.system(f"python3 ger_4_txt2parse.py {input_path}")
-    os.system(f"python3 ger_5_txt2conll.py {input_path}")
 
 
 def run_docker_segmenter(input_path, datapath="data/de"):
     print("Running segmentation in Docker (Python 2)")
     subprocess.run([
-        "docker", "run", "-it", "--rm",
+        "docker", "run", "--rm",
         "-v", os.getcwd() + ":/home/DPLP",
         "-w", "/home/DPLP",
         "mohamadisara20/dplp-env:ger",
@@ -70,7 +73,7 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    run_local_preprocessing(args.input_folder)
+    run_optimized_preprocessing(args.input_folder)
     run_docker_segmenter(args.input_folder, args.corpus_base_path)
     extract_edus(args.input_folder, args.output_folder)
     print(f"Finished! EDU .txt files are in: {args.output_folder}")
